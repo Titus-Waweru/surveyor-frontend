f203c11e78d2de2717fdb99a38822825ca3655c5
Titus Waweru <tituswaweru631@gmail.com>
2025-05-28 16:14:18 +0300

    Fix: Updated axios.js to match .env

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,7 @@
-// utils/axios.js
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
